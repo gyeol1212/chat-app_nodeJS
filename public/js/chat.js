a8f5abffc3a37776bdca8481c019fd23fcfe7283
gyeol <gyeol1212@likelion.org>
2019-08-17 15:29:56 +0900

    chat-app 완성

--- public/js/chat.js
+++ public/js/chat.js
@@ -7,39 +7,77 @@ const $messages = document.getElementById('messages');
 
 const messageTemplate = document.getElementById('message-template').innerHTML;
 const locationTemplate = document.getElementById('location-template').innerHTML;
+const sidebarTemplate = document.getElementById('sidebar-template').innerHTML;
 
 const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true });
+
+const autoscroll = () => {
+  const $newMessage = $messages.lastElementChild;
+
+  // height of the new message
+  const newMessageStyles = getComputedStyle($newMessage);
+  const newMessageMargin = parseInt(newMessageStyles.marginBottom);
+  const newMessageHeight = $newMessage.offsetHeight + newMessageMargin;
+
+  // visible height
+  const visibleHeight = $messages.offsetHeight;
+
+  // Height of messages container
+  const containerHeight = $messages.scrollHeight;
+
+  // How far have I scrolled
+  const scrollOffset = $messages.scrollTop + visibleHeight;
+
+  if (containerHeight - newMessageHeight <= scrollOffset) {
+    $messages.scrollTop = $messages.scrollHeight;
+  }
+};
 
 socket.on('message', message => {
   console.log(message);
   const html = Mustache.render(messageTemplate, {
+    username: message.username,
     message: message.text,
     createdAt: moment(message.createdAt).format('h:mm a'),
   });
   $messages.insertAdjacentHTML('beforeend', html);
+  autoscroll();
 });
 
 socket.on('locationMessage', url => {
   console.log(url);
   const html = Mustache.render(locationTemplate, {
+    username: url.username,
     url: url.url,
     createdAt: moment(url.createdAt).format('h:mm a'),
   });
   $messages.insertAdjacentHTML('beforeend', html);
+  autoscroll();
+});
+
+socket.on('roomData', ({ room, users }) => {
+  const html = Mustache.render(sidebarTemplate, {
+    room,
+    users,
+  });
+
+  document.getElementById('sidebar').innerHTML = html;
 });
 
 $messageButton.addEventListener('click', e => {
   e.preventDefault();
 
-  $messageButton.setAttribute('disabled', 'disabled');
-
   const message = $input.value;
-  socket.emit('sendMessage', message, error => {
-    $messageButton.removeAttribute('disabled');
-    $input.value = '';
 
-    console.log(error || 'Message delivered');
-  });
+  if (message) {
+    $messageButton.setAttribute('disabled', 'disabled');
+    socket.emit('sendMessage', message, error => {
+      $messageButton.removeAttribute('disabled');
+      $input.value = '';
+
+      console.log(error || 'Message delivered');
+    });
+  }
 });
 
 $locationButton.addEventListener('click', () => {
@@ -60,4 +98,9 @@ $locationButton.addEventListener('click', () => {
   });
 });
 
-socket.emit('join', { username, room });
+socket.emit('join', { username, room }, error => {
+  if (error) {
+    alert(error);
+    location.href = '/';
+  }
+});
